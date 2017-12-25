20b12a4e5a50b480a6724f9a9f7aa4c55661e039
Yoelnacho <yoelnacho@gmail.com>
2017-12-25 20:24:44 -0300

    Nuevos componentes y ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 // Material
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+ 
+// Components
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PopularComponent } from './popular/popular.component';
 
+// Services
+import { ContactsService } from './contacts.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PopularComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent
+  },
+  {
+    path: 'popular',
+    component: PopularComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    PopularComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule, 
-    MatCheckboxModule
+    MatCheckboxModule,
+    RouterModule.forRoot(routes)
   ],
   exports: [
     MatButtonModule, 
     MatCheckboxModule
   ],
-  providers: [],
+  providers: [ContactsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
